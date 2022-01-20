c6b4f53ef4d72bc7d5214ab8ef42c357386be85e
j1642 <60148902+j1642@users.noreply.github.com>
2022-01-20 11:45:36 -0500

    Updated pieces, board, game

--- chess/game.py
+++ chess/game.py
@@ -1,10 +1,11 @@
+import board
+
 class Game:
     '''Should this stuff stay as a class or become the main program?'''
     def __init__(self):
         self.player_color = ''
         self.computer_color = ''
         self.turn = ''
-        self.pieces_on_board = []
         
         Game.select_color(self)
         
@@ -28,66 +29,31 @@ class Game:
         else:
             return 'Input <white> or <black>.'
     
+    # TODO
     # Algorithms for the computer to make a move
     def computer_move():
         pass
         # .....
         self.turn = 'player'
         self.between_moves()
-        
+    
+    # TODO
     def player_move():
-        pass
-        #.........
+        old_square = input('Square to move from? (int)')
+        new_square = input('Square to move to? (int)')
+        symbol = board.squares[old_square]
+        
         self.turn = 'computer'
         self.between_moves()
-
+        
+    # TODO
     def between_moves():
         self.in_check()
         #update_moves for all pieces in pieces_on_board
     
     # TODO: finish
     def in_check(self):
-        # if square[king_black] in Queen_white.moves, Pawn1_white.moves, etc:
+        # if square[king_black] in [white_controlled_squares]:
             # 
     
-    # TODO: finish
-    # Should this be merged with initializing squares in the Board class?
-    #***** Maybe move this into the initialize board Board method
-    #           b.white_q style objects******
-    # Are these decent variable names?
-    def initialize_pieces(self):
-        white_rq = Rook('rook a1', 'white', 0)
-        white_nq = Knight('knight b1', 'white', 1)
-        white_bq = Bishop('bishop c1', 'white', 2)
-        white_q = Queen('queen d1', 'white', 3)
-        white_k = King('king e1', 'white', 4)
-        white_bk = Bishop('bishop f1', 'white', 5)
-        white_nk = Knight('knight g1', 'white', 6)
-        white_rk = Rook('rook h1', 'white', 7)
-        
-        white_pa = Pawn('pawn a2', 'white', 8)
-        white_pb = Pawn('pawn b2', 'white', 9)
-        white_pc = Pawn('pawn c2', 'white', 10)
-        white_pd = Pawn('pawn d2', 'white', 11)
-        white_pe = Pawn('pawn e2', 'white', 12)
-        white_pf = Pawn('pawn f2', 'white', 13)
-        white_pg = Pawn('pawn g2', 'white', 14)
-        white_ph = Pawn('pawn h2', 'white', 15)
-        
-        black_rq = Rook('rook a8', 'black', 56)
-        black_nq = Knight('knight b8', 'black', 57)
-        black_bq = Bishop('bishop c8', 'black', 58)
-        black_q = Queen('queen d8', 'black', 59)
-        black_k = King('king e8', 'black', 60)
-        black_bk = Bishop('bishop f8', 'black', 61)
-        black_nk = Knight('knight g8', 'black', 62)
-        black_rk = Rook('rook h8', 'black', 63)
-        
-        black_pa = Pawn('pawn a7', 'black', 48)
-        black_pb = Pawn('pawn b7', 'black', 49)
-        black_pc = Pawn('pawn c7', 'black', 50)
-        black_pd = Pawn('pawn d7', 'black', 51)
-        black_pe = Pawn('pawn e7', 'black', 52)
-        black_pf = Pawn('pawn f7', 'black', 53)
-        black_pg = Pawn('pawn g7', 'black', 54)
-        black_ph = Pawn('pawn h7', 'black', 55)
+    
--- chess/pieces.py
+++ chess/pieces.py
@@ -1,3 +1,5 @@
+import board
+
 class Pawn:
     def __init__(self, name: str, white_or_black: str, position: int):
         self.name = name
@@ -7,26 +9,30 @@ class Pawn:
         self.has_moved = False
         self.giving_check = False
         
+    def __repr__(self):        
+        return f'''Sym:, {self.symbol}, Sq:, {self.square}, {self.color},
+    has_moved: {self.has_moved}'''
+        
     # TODO: Allow diagonal captures, en passant
     # TODO: Remove moves where a friendly piece is
     def update_moves(self):
-        if has_moved is True:
-            if self.color = 'white':
+        if self.has_moved is True:
+            if self.color == 'white':
                 self.moves = list(self.square + 8)
-            elif self.color = 'black':
+            elif self.color == 'black':
                 self.moves = list(self.square - 8)
-        elif has_moved is False:
+        elif self.has_moved is False:
             self.moves = list(self.square + 8, self.square + 16)
     
     # Where squares is a global list of square contents
     # Get new_square from input in main script
-    def move_piece(self):
-        if new_square in self.moves:
-            old_square = self.square
-            self.square = new_square
-            squares[old_square], squares[new_square] = '', self.name
-        else:
-            return f'Not a valid move for {self.name}.'
+   # def move_piece(self):
+    #    if new_square in self.moves:
+     #       old_square = self.square
+      #      self.square = new_square
+       #     squares[old_square], squares[new_square] = ' ', self.name
+        #else:
+         #   return f'Not a valid move for {self.name}.'
 
 
 class Knight:
@@ -41,6 +47,9 @@ class Knight:
             self.symbol = 'N'
         elif self.color == 'black':
             self.symbol = 'n'
+            
+    def __repr__(self):        
+        return f'Sym:, {self.symbol}, Sq:, {self.square}, {self.color}'
         
     # TODO: Remove moves where a friendly piece is
     # Knight movement is very dependent on current square
@@ -94,14 +103,12 @@ class Knight:
     # Get new_square from input in main script
     # Squares is now an attribute, <board_object>.squares
     def move_piece(self, new_square: int):
-        global squares
-        
         if new_square in self.moves:
             old_square = self.square
             self.square = new_square
             
             # squares not working
-            squares[old_square], squares[new_square] = '', self.symbol
+     #       squares[old_square], squares[new_square] = '', self.symbol
         else:
             print(f'Not a valid move for {self.name}.')
             print(f'Not a valid move for {self.__class__.__name__}.')
@@ -120,6 +127,9 @@ class Bishop:
             self.symbol = 'B'
         elif self.color == 'black':
             self.symbol = 'b'
+            
+    def __repr__(self):        
+        return f'Sym:, {self.symbol}, Sq:, {self.square}, {self.color}'
         
     # TODO: Remove moves where friendly pieces are
     # TODO: Remove moves that jump over pieces
@@ -139,7 +149,7 @@ class Bishop:
         if new_square in self.moves:
             old_square = self.square
             self.square = new_square
-            squares[old_square], squares[new_square] = '', self.name
+    #        squares[old_square], squares[new_square] = '', self.name
         else:
             return f'Not a valid move for {self.name}.'
 
@@ -150,12 +160,17 @@ class Rook:
         self.color = white_or_black
         self.square = position
         self.moves = []
+        self.has_moved = False
         self.giving_check = False
         
         if self.color == 'white':
             self.symbol = 'R'
         elif self.color == 'black':
             self.symbol = 'r'
+            
+    def __repr__(self):        
+        return f'''Sym:, {self.symbol}, Sq:, {self.square}, {self.color}, 
+    has_moved: {self.has_moved}'''
         
     # TODO: Remove moves where friendly pieces are
     # TODO: Remove moves that jump over pieces
@@ -172,7 +187,8 @@ class Rook:
         if new_square in self.moves:
             old_square = self.square
             self.square = new_square
-            squares[old_square], squares[new_square] = '', self.name
+            self.has_moved = True
+    #        squares[old_square], squares[new_square] = '', self.name
         else:
             print(f'Not a valid move for {self.name}.')
             return f'Not a valid move for {self.name}.'
@@ -190,6 +206,9 @@ class Queen:
             self.symbol = 'Q'
         elif self.color == 'black':
             self.symbol = 'q'
+            
+    def __repr__(self):        
+        return f'Sym:, {self.symbol}, Sq:, {self.square}, {self.color}'
         
     # TODO: Remove moves where friendly pieces are
     # TODO: Remove moves that jump over pieces
@@ -213,7 +232,7 @@ class Queen:
         if new_square in self.moves:
             old_square = self.square
             self.square = new_square
-            squares[old_square], squares[new_square] = '', self.name
+   #         squares[old_square], squares[new_square] = '', self.name
         else:
             return f'Not a valid move for {self.name}.'
 
@@ -224,12 +243,17 @@ class King:
         self.color = white_or_black
         self.square = position
         self.moves = []
+        self.has_moved = False
         self.in_check = False
         
         if self.color == 'white':
             self.symbol = 'K'
         elif self.color == 'black':
             self.symbol = 'k'
+            
+    def __repr__(self):
+        return f'''Sym:, {self.symbol}, Sq:, {self.square}, {self.color}, 
+    moved:, {self.has_moved}, in check:, {self.in_check}'''
         
     # TODO: Remove moves where friendly pieces are
     # TODO: Remove enemy-controlled squares
@@ -237,14 +261,55 @@ class King:
         all_moves = []
         for delta in [7, 8, 9, -1, 1, -9, -8, -7]:
             if self.square + delta >= 0:
-                all_moves.append(self.square + delta)            
+                all_moves.append(self.square + delta)         
+        
+        # TODO: Finish Castling - get rook to move as well
+        if self.has_moved is False:
+            if self.color == 'white':
+                # Castle kingside
+                if board.white_rk.has_moved is False \
+                    and board.squares[5] == board.squares[6] == ' ':
+                        all_moves.append(6)
+                # Castle queenside
+                if board.white_rq.has_moved is False \
+                    and board.squares[1] == board.squares[2] == ' ':
+                        all_moves.append(2)
+            elif self.color == 'black':
+                # Castle kingside
+                if board.black_rk.has_moved is False \
+                    and board.squares[61] == board.squares[62] == ' ':
+                        all_moves.append(62)
+                # Castle queenside
+                if board.black_rq.has_moved is False \
+                    and board.squares[57] == board.squares[58] == ' ':
+                        all_moves.append(58)
+                        
+        # Remove illegal king moves into opponent controlled squares
+        all_moves_set = set(all_moves)
+        all_moves = list(all_moves_set)
+        
+        if self.color == 'white':
+            for illegal_move in board.black_controlled_squares():
+                try:
+                    all_moves.remove(illegal_move)
+                except ValueError:
+                    pass
+                
+        elif self.color == 'black':
+            for illegal_move in board.white_controlled_squares():
+                try:
+                    all_moves.remove(illegal_move)
+                except ValueError:
+                    pass
+                
         self.moves = all_moves
     
     def move_piece(self, new_square: int):
         if new_square in self.moves:
             old_square = self.square
             self.square = new_square
-            squares[old_square], squares[new_square] = '', self.name
+            self.has_moved = True
+   #         squares[old_square], squares[new_square] = '', self.name
         else:
             print(f'Not a valid move for {self.name}.')
             return f'Not a valid move for {self.name}.'
